5368c9bdafd14fa44e338e4d14b0e048fb9a6a5d
Ritwik Pai <ritwikpai777@gmail.com>
2024-12-15 02:16:20 +0530

    Fixes

--- src/App.js
+++ src/App.js
@@ -34,28 +34,30 @@ function App() {
         <button onClick={clickHandler}>Open Form</button>
         {isOpen && (
           <div className="modal" onClick={closeHandler}>
-            <form onSubmit={submitHandler}>
-              <h2>Fill Details</h2>
-              <div className="input-group">
-                <label htmlFor="username">Username: </label>
-                <input type="text" name="username" id="username" />
-              </div>
-              <div className="input-group">
-                <label htmlFor="email">Email Address:</label>
-                <input type="email" name="email" id="email" required />
-              </div>
-              <div className="input-group">
-                <label htmlFor="phoneNo">Phone Number:</label>
-                <input type="number" name="phoneNo" id="phone" required />
-              </div>
-              <div className="input-group">
-                <label htmlFor="dob">Date of Birth:</label>
-                <input type="date" name="dob" id="dob" />
-              </div>
-              <button type="submit" className="submit-button">
-                Submit
-              </button>
-            </form>
+            <div className="modal-content">
+              <form onSubmit={submitHandler}>
+                <h2>Fill Details</h2>
+                <div className="input-group">
+                  <label htmlFor="username">Username: </label>
+                  <input type="text" name="username" id="username" />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="email">Email Address:</label>
+                  <input type="email" name="email" id="email" required />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="phoneNo">Phone Number:</label>
+                  <input type="number" name="phoneNo" id="phone" required />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="dob">Date of Birth:</label>
+                  <input type="date" name="dob" id="dob" />
+                </div>
+                <button type="submit" className="submit-button">
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
         )}
       </div>
